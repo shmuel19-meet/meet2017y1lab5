9ca853e993312f96d6b37b8cedb0f737e55c16c3
shmuel19-meet <shmuel19@meet.mit.edu>
2017-07-27 14:21:44 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,12 @@
+def add_numbers(start,end):
+    c=0
+    for number in range(start,end+1):
+        print(number)
+        c=c+number
+    return c
+
+#using the function
+test1= add_numbers(333,778)
+print(test1)
+
+
